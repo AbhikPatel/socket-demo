97b6cd5f03871345ad592da79f02c20850e73804
AbhikPatel <abhikpatel15@gmail.com>
2023-02-10 20:20:56 +0530

    created app

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SocketService } from './socket.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    SocketService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
