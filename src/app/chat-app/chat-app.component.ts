7a06630dea90c8050e79205699094b72c2207310
AbhikPatel <abhikpatel15@gmail.com>
2023-02-15 18:32:09 +0530

    feat: set username

--- src/app/chat-app/chat-app.component.ts
+++ src/app/chat-app/chat-app.component.ts
@@ -21,9 +21,10 @@ export class ChatAppComponent implements OnInit {
   }
 
   public props(){
-    // this._service.listen('welcome').subscribe((data) => console.log(data))
+    this._service.listen('welcome').subscribe((data) => console.log(data))
     this._service.listen('alive').subscribe((data) => this.users = data.sids)
     this._service.listen('chat').subscribe((data) => console.log(data))
+    this._service.emit('setUserName', localStorage.getItem('name'))
   }
 
   public getMessage(message:any){
